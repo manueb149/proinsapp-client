ea2fe02ae3d84f0a4ee67d7435b4848e518c9c89
Manuel Bencosme <manueb149@gmail.com>
2020-12-06 22:17:34 -0400

    fix resports.style.js

--- src/layout/Reports/Reports.style.js
+++ src/layout/Reports/Reports.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ReportsContainer = styled.div`
-    td.MuiTableCell-root.MuiTableCell-body.MUIDataTableSelectCell-root-35.MUIDataTableSelectCell-fixedLeft-37.MuiTableCell-paddingCheckbox{
+    .MUIDataTableSelectCell-fixedLeft-37.MuiTableCell-paddingCheckbox{
         background-color: #fff;
     }
 `;
