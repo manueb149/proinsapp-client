cfd8e76e0c7dda5038677ab3ac97411526417a30
Manuel Bencosme <manueb149@gmail.com>
2021-01-13 21:13:22 -0400

    change to production

--- src/config/http-common.js
+++ src/config/http-common.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   // Production
-  // baseURL: "https://young-retreat-94433.herokuapp.com/api",
+  baseURL: "https://young-retreat-94433.herokuapp.com/api",
 
   // Development
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
 
   // validateStatus: function (status) {
   //   return status >= 200 && status <= 503;
