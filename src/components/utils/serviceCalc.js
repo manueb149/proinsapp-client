443fb2c54b2a063544b58c888b85de3d104fa3bb
Manuel Bencosme <manueb149@gmail.com>
2021-01-13 21:26:57 -0400

    fix serviceCalc.js

--- src/components/utils/serviceCalc.js
+++ src/components/utils/serviceCalc.js
@@ -2,17 +2,17 @@ const summaryCalc = (data, values, servicesType, servicesTypeCk, detailSinister,
     let total = 0;
     if (Number(data.distancia) <= 15) {
         total = 1200;
-        if (servicesTypeCk.EX) total = total + Number(servicesType.EX);
-        if (servicesTypeCk.CR) total = total + Number(servicesType.CR);
-        if (servicesTypeCk.CG) total = total + Number(servicesType.CG);
-        if (servicesTypeCk.CE) total = total + Number(servicesType.CE);
-        if (servicesTypeCk.SG) total = total + Number(servicesType.SG);
-        if (servicesTypeCk.PE) total = total + Number(servicesType.PE);
+        if (servicesTypeCk.EX && Number(servicesType.EX)>=0) total = total + Number(servicesType.EX);
+        if (servicesTypeCk.CR && Number(servicesType.CR)>=0) total = total + Number(servicesType.CR);
+        if (servicesTypeCk.CG && Number(servicesType.CG)>=0) total = total + Number(servicesType.CG);
+        if (servicesTypeCk.CE && Number(servicesType.CE)>=0) total = total + Number(servicesType.CE);
+        if (servicesTypeCk.SG && Number(servicesType.SG)>=0) total = total + Number(servicesType.SG);
+        if (servicesTypeCk.PE && Number(servicesType.PE)>=0) total = total + Number(servicesType.PE);
 
-        if (detailSinisterCk.VO) total = total + Number(detailSinister.VO);
-        if (detailSinisterCk.IN) total = total + Number(detailSinister.IN);
-        if (detailSinisterCk.CO) total = total + Number(detailSinister.CO);
-        if (detailSinisterCk.DM) total = total + Number(detailSinister.DM);
+        if (detailSinisterCk.VO && Number(servicesType.VO)>=0) total = total + Number(detailSinister.VO);
+        if (detailSinisterCk.IN && Number(servicesType.IN)>=0) total = total + Number(detailSinister.IN);
+        if (detailSinisterCk.CO && Number(servicesType.CO)>=0) total = total + Number(detailSinister.CO);
+        if (detailSinisterCk.DM && Number(servicesType.DM)>=0) total = total + Number(detailSinister.DM);
 
         if (data.dia === "DF") total += total * Number(values.FF) / 100;
         if (servicesTypeCk.SP) {
@@ -20,21 +20,21 @@ const summaryCalc = (data, values, servicesType, servicesTypeCk, detailSinister,
                 total += total * Number(servicesType.SP0) / 100;
             }
         }
-        if (servicesTypeCk.LM) total = total + Number(servicesType.LM) * Number(servicesType.SL);
+        if (servicesTypeCk.LM && Number(servicesType.LM)>=0) total = total + Number(servicesType.LM) * Number(servicesType.SL);
         if (data.noche) total += total * Number(servicesType.TN) / 100;
     } else {
         total = 1200 + ((Number(data.distancia) - 15) * Number(servicesType.TG));
-        if (servicesTypeCk.EX) total = total + Number(servicesType.EX);
-        if (servicesTypeCk.CR) total = total + Number(servicesType.CR);
-        if (servicesTypeCk.CG) total = total + Number(servicesType.CG);
-        if (servicesTypeCk.CE) total = total + Number(servicesType.CE);
-        if (servicesTypeCk.SG) total = total + Number(servicesType.SG);
-        if (servicesTypeCk.PE) total = total + Number(servicesType.PE);
+        if (servicesTypeCk.EX && Number(servicesType.EX)>=0) total = total + Number(servicesType.EX);
+        if (servicesTypeCk.CR && Number(servicesType.CR)>=0) total = total + Number(servicesType.CR);
+        if (servicesTypeCk.CG && Number(servicesType.CG)>=0) total = total + Number(servicesType.CG);
+        if (servicesTypeCk.CE && Number(servicesType.CE)>=0) total = total + Number(servicesType.CE);
+        if (servicesTypeCk.SG && Number(servicesType.SG)>=0) total = total + Number(servicesType.SG);
+        if (servicesTypeCk.PE && Number(servicesType.PE)>=0) total = total + Number(servicesType.PE);
 
-        if (detailSinisterCk.VO) total = total + Number(detailSinister.VO);
-        if (detailSinisterCk.IN) total = total + Number(detailSinister.IN);
-        if (detailSinisterCk.CO) total = total + Number(detailSinister.CO);
-        if (detailSinisterCk.DM) total = total + Number(detailSinister.DM);
+        if (detailSinisterCk.VO && Number(servicesType.VO)>=0) total = total + Number(detailSinister.VO);
+        if (detailSinisterCk.IN && Number(servicesType.IN)>=0) total = total + Number(detailSinister.IN);
+        if (detailSinisterCk.CO && Number(servicesType.CO)>=0) total = total + Number(detailSinister.CO);
+        if (detailSinisterCk.DM && Number(servicesType.DM)>=0) total = total + Number(detailSinister.DM);
 
         if (data.dia === "DF") total += total * Number(values.FF) / 100;
         if (servicesTypeCk.SP) {
@@ -42,7 +42,7 @@ const summaryCalc = (data, values, servicesType, servicesTypeCk, detailSinister,
                 total = total + Number(servicesType.TG) * Number(servicesType.SP) * (1 + Number(servicesType.SP0) / 100);
             }
         }
-        if (servicesTypeCk.LM) total = total + (Number(servicesType.SL) + Number(servicesType.TG)) * Number(servicesType.LM);
+        if (servicesTypeCk.LM && Number(servicesType.LM)>=0) total = total + (Number(servicesType.SL) + Number(servicesType.TG)) * Number(servicesType.LM);
         if (data.noche) total += total * Number(servicesType.TN) / 100;
     }
     data.precio = total.toFixed(2);
